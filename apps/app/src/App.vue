42737f796eeefd8c98339a43687fa9cb2fbcdbea
Brent den Boer <brent@brentify.nl>
2024-04-24 23:02:41 +0200

    feat(app): Added name input

--- apps/app/src/App.vue
+++ apps/app/src/App.vue
@@ -4,6 +4,7 @@
       <div class="content pa-5 ma-auto w-100 h-100 d-flex justify-center align-center flex-column">
         <h1 class="text-md-h2 mb-12 text-sm-h3">BrentTV ticket</h1>
         <v-form class="w-100 d-flex flex-column">
+          <v-text-field label="Naam" class="mb-2"/>
           <v-text-field label="Titel probleem"/>
           <v-textarea auto-grow label="Omschrijving probleem"/>
           <p class="text-subtitle-1 text-center align-self-center mb-2">Hoe hard ga je huilen als ik het niet op
